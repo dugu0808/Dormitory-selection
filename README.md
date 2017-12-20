8ca07870038fd90bec6f8f60416591710b2862e2
Liu Ke <450234523@qq.com>
2017-12-20 23:28:23 +0800

    add a few layout files

---
+++ README.md
@@ -0,0 +1,3 @@
+## Dormitory Selection 
+
+This is a student online selection of dormitory selection application. 
---
+++ app/src/main/java/me/keliu/dormitory_selection/MainActivity.java
@@ -0,0 +1,13 @@
+package me.keliu.dormitory_selection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
